0c41d8e73c7bc6c2d69d6e731f5868648a7878ca
Cho Hye Min <pedalioaspare@gmail.com>
2018-07-23 10:03:41 -0700

    Interp varying frame interp list

--- utils.py
+++ utils.py
@@ -398,11 +398,13 @@ def generate_continuous_random_interps(sess, dcgan, config, total_frame_num, is_
         z2 = np.asarray(rand_batch_z[1, :])
 
         if is_var_interp_num:
-            rand = random.random()
-            inv_rand = 1.0 - (rand * rand)
-            steps_per_interp = lerp(inv_rand, 128*4, 16)
+            # rand = random.random()
+            # inv_rand = 1.0 - (rand * rand)
+            # steps_per_interp = lerp(inv_rand, 128*4, 16)
+            interp_frame_nums = [8, 16, 32, 10, 30, 20, 30, 34, 50, 25, 50, 100, 120, 250, 300, 512]
+            steps_per_interp = interp_frame_nums[random.randint(0, len(interp_frame_nums)-1)]
             # steps_per_interp = random.randint(16, 128 * 4)
-            
+
         # z2 = np.random.uniform(-1, 1, size=(1 , dcgan.z_dim))[0]
         print("MEEE newly assigned z1: " + str(z1))
         print("MEEE newly gen uniform z2: " + str(z2))
